5571b4505240dcd0e40159339894c9607472a4b4
AnnVinogradova <125795542+AnnVinogradova@users.noreply.github.com>
2023-06-07 21:27:31 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CharactersComponent } from './characters/Characters.component';
+import { LocationsComponent } from './locations/Locations.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: 'locations', component: LocationsComponent },
+  { path: '', component: CharactersComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    CharactersComponent,
+    LocationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [CharactersComponent]
+})
+export class AppModule { }
